b2357b0708b95e00114d67b0d9a0542b0e0d7df4
Tofigh-Tevfik <116098153+Tofigh-Tevfik@users.noreply.github.com>
2023-02-18 00:08:25 +0300

    Update process.py

--- PLC OOP Device Control/process.py
+++ PLC OOP Device Control/process.py
@@ -14,11 +14,25 @@ class Process:
         deviceState["deviceProvider"] = DeviceProvider(eConfig, numberOfDevices)
         self.deviceControl = DeviceControl()
 
+    # When index is set to False it will set the commands for every Device
+    # It calls a method from deviceControl object that sets the props for devices
+    # the command must be a dictionary with this structure
+    # {
+    #      "enable": ...
+    #       "reset" : ...
+    #       "hasError": ...
+    #       "isReady" : ...
+    #       "errorId" : ...
+    #       "initialized": ...
+    # }
     def setCommands(self, props, index = False):
         self.deviceControl.setCommands(props, index)
 
+    # Again when index is set to false it will print out the props of all devices
+    # it calls a method from deviceControl object
     def readProps(self, index = False):
         self.deviceControl.readProps(index)
-
+    # adds devices by a given configuration
+    # Config is a list of strings with device names like ["A", "B", "C"]
     def addDevice(self, Config):
         deviceState["deviceProvider"].addDevice(Config)
